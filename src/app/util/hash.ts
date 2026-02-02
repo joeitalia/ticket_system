f99084fa72d9ef07d7382e38496e00998ab5eb0f
Jojo Italia <102933802+joeitalia@users.noreply.github.com>
2026-02-02 19:37:36 +0800

    Delete src/app/util directory

--- src/app/util/hash.ts
+++
@@ -1,7 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const hashText = async (text: string) => {
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(text, salt);
-  return hash;
-}
